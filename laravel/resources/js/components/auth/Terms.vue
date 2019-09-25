e25fd6b198eb6a11b91efe64f3767db61696686c
Ben Bjurstrom <ben@jelled.com>
2019-09-24 23:00:18 -0700

    Add privacy and terms

---
+++ laravel/resources/js/components/auth/Terms.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="whitespace-pre-line">
+    Jelled Media, LLC Terms of Service
+
+    1. Terms
+    By accessing the website at http://astrify.com, you are agreeing to be bound by these terms of service, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this website are protected by applicable copyright and trademark law.
+
+    2. Use License
+    Permission is granted to temporarily download one copy of the materials (information or software) on Jelled Media, LLC's website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:
+
+    <ol class="list-disc px-6">
+      <li>modify or copy the materials;</li>
+      <li>use the materials for any commercial purpose, or for any public display (commercial or non-commercial);</li>
+      <li>attempt to decompile or reverse engineer any software contained on Jelled Media, LLC's website;</li>
+      <li>remove any copyright or other proprietary notations from the materials; or</li>
+      <li>transfer the materials to another person or "mirror" the materials on any other server.</li>
+    </ol>
+    This license shall automatically terminate if you violate any of these restrictions and may be terminated by Jelled Media, LLC at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.
+
+    3. Disclaimer
+    The materials on Jelled Media, LLC's website are provided on an 'as is' basis. Jelled Media, LLC makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties including, without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights.
+    Further, Jelled Media, LLC does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its website or otherwise relating to such materials or on any sites linked to this site.
+
+    4. Limitations
+    In no event shall Jelled Media, LLC or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the materials on Jelled Media, LLC's website, even if Jelled Media, LLC or a Jelled Media, LLC authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.
+
+    5. Accuracy of materials
+    The materials appearing on Jelled Media, LLC's website could include technical, typographical, or photographic errors. Jelled Media, LLC does not warrant that any of the materials on its website are accurate, complete or current. Jelled Media, LLC may make changes to the materials contained on its website at any time without notice. However Jelled Media, LLC does not make any commitment to update the materials.
+
+    6. Links
+    Jelled Media, LLC has not reviewed all of the sites linked to its website and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by Jelled Media, LLC of the site. Use of any such linked website is at the user's own risk.
+
+    7. Modifications
+    Jelled Media, LLC may revise these terms of service for its website at any time without notice. By using this website you are agreeing to be bound by the then current version of these terms of service.
+
+    8. Governing Law
+    These terms and conditions are governed by and construed in accordance with the laws of California and you irrevocably submit to the exclusive jurisdiction of the courts in that State or location.
+  </div>
+</template>
